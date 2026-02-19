662aa6eaf7403e59e3dc33cb5eeffc38218ea05e
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-19 13:21:12 -0800

    Update EventsTab.jsx

--- todo-list/src/components/EventsTab.jsx
+++ todo-list/src/components/EventsTab.jsx
@@ -4,11 +4,27 @@ import { addEvent, deleteEvent } from "../redux/eventsSlice";
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+const [title, setTitle] = useState('');
+const [day, setDay] = useState('Sunday');
+const [time, setTime] = useState('');
+
+
 
 return (
     <div>
         <h2>Weekly Events</h2>
+        <form onSubmit ={handleSubmit}>
+            <input
+                type="text"
+                placeholder ="Event Name"
+                vaule = {title}
+                onChange={(e)=>}
+                />
 
+
+
+
+        </form>
 
 
 
