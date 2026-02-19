c8fe41d64ed94da1334f0dde1321d0c15f2c80fd
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-19 13:26:08 -0800

    updating to show divs, and get events

--- todo-list/src/components/EventsTab.jsx
+++ todo-list/src/components/EventsTab.jsx
@@ -3,10 +3,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { addEvent, deleteEvent } from "../redux/eventsSlice";
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+function EventsTab(){
+    const [title, setTitle] = useState('');
+    const [day, setDay] = useState('Sunday');
+    const [time, setTime] = useState('');
 
-const [title, setTitle] = useState('');
-const [day, setDay] = useState('Sunday');
-const [time, setTime] = useState('');
+    const dispatch = useDispatch();
+    const events = useSelector((state) => state.events);
+    
+}
 
 
 
@@ -17,8 +22,8 @@ return (
             <input
                 type="text"
                 placeholder ="Event Name"
-                vaule = {title}
-                onChange={(e)=>}
+                value = {title}
+                onChange={(e) => setTitle(e.target.value)}
                 />
 
 
@@ -26,8 +31,23 @@ return (
 
         </form>
 
+        <div className="week-grid">
+            {days.map((d)=>(
+                <div key={d} className="day-column">
+                    <h3>{d}</h3>
+                    {events
+                        .filter((e)=> e.day ===d)
+                        .map((e)=>(
+                            <div key={e.id} className="event-chip">
+                                <span>{title}</span>
+                                {e.title &&<span className ="event-time">{e.time}</span>}
+                                <button onClick={()=>dispatchEvent(deleteEvent(e.id))}>X</button>
 
-
+                            </div>
+                        ))}
+                        </div>
+            ))}
+        </div>
 
     </div>
 );
