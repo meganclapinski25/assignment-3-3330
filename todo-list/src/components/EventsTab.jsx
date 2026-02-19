3d3be326126174cc64a2dfbff0549cb6efbf11d0
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-19 13:16:53 -0800

    creating events page

---
+++ todo-list/src/components/EventsTab.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addEvent, deleteEvent } from "../redux/eventsSlice";
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+return (
+    <div>
+        <h2>Weekly Events</h2>
+
+
+
+
+
+    </div>
+);
